9a3b23c087421ee31f8e638b377fadec71c5fa19
Yusrina Mastura <152014587+yusrinaa93@users.noreply.github.com>
2026-02-10 23:28:51 +0700

    fix migrations jadwal

--- database/migrations/2025_10_13_195415_create_attendances_table.php
+++ database/migrations/2025_10_13_195415_create_attendances_table.php
@@ -6,24 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-public function up(): void
-{
-    Schema::create('attendances', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('user_id')->constrained()->onDelete('cascade');
-        $table->foreignId('schedule_id')->constrained()->onDelete('cascade');
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('presensi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('schedule_id')->constrained('jadwal')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('attendances');
+        Schema::dropIfExists('presensi');
     }
 };
